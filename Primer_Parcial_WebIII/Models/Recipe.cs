99ed9df70e24160e66221e4213188161b560b147
Manuel8973 <mineriajomi@gmail.com>
2025-09-25 19:58:44 -0400

    corrigiendo

--- Primer_Parcial_WebIII/Models/Recipe.cs
+++ Primer_Parcial_WebIII/Models/Recipe.cs
@@ -1,6 +1,32 @@
-﻿namespace PrimerParcial.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
 {
     public class Recipe
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public int PreparationTimeMinutes { get; set; }
+        public int Servings { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        // Clave foránea y navegación a Category
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        // Relación con ingredientes
+        public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
     }
 }
