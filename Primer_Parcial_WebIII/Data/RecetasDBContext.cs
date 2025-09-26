ebb102e29f30cfc8b63f9c45cb5a9bfd980b93b5
Manuel8973 <mineriajomi@gmail.com>
2025-09-25 20:08:30 -0400

    Agregacion de class Category

--- Primer_Parcial_WebIII/Data/RecetasDBContext.cs
+++ Primer_Parcial_WebIII/Data/RecetasDBContext.cs
@@ -37,4 +37,17 @@ namespace PrimerParcial.Data
             base.OnModelCreating(modelBuilder);
         }
     }
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; } // Ejemplo: "Postres", "Platos Principales"
+
+        public string Description { get; set; }
+
+        // Relación con recetas
+        public ICollection<Recipe> Recipes { get; set; } = new List<Recipe>();
+    }
 }
